81cfcc3f6210d21461fba3efc4050bd05e488075
MehediAhsan <mdmehedicse01@gmail.com>
2024-02-19 15:34:25 +0600

    changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 'use client'
+import Navbar from "@/components/Navbar"
+import Link from "next/link"
 import { useState } from "react"
+import { FaBackward } from "react-icons/fa"
 
 export default function Home() {
-  const [path, setPath] = useState()
+  const [open, setOpen] = useState(false)
   return (
+    <>
+    <Navbar/>
     <main className="flex justify-center items-center pt-52">
-      <button className="bg-teal-900 p-2">
+      <button onClick={() => setOpen(true)} className={`bg-teal-900 p-2 ${open? 'hidden': 'block'}`}>
         click me 🧐
       </button>
+      <div className={`flex items-center gap-2 p-2 text-gray-200 ${open? 'block': 'hidden'}`}>
+        <FaBackward onClick={() => setOpen(false)} className="mr-6 text-teal-600 cursor-pointer"></FaBackward>
+        <Link href="/mehedi">Mehedi</Link>
+        <span>/</span>
+        <Link href="/tauhid">Tauhid</Link>
+      </div>
     </main>
+    </>
   )
 }
