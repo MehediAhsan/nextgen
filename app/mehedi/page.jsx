75613f30e16e5c65a50a477db974315bb3d78f39
MehediAhsan <mdmehedicse01@gmail.com>
2024-02-20 17:28:53 +0600

    debounced input

--- app/mehedi/page.jsx
+++ app/mehedi/page.jsx
@@ -1,11 +1,13 @@
 'use client'
 import React from 'react';
 import DynamicTable from '../../components/reuse/DynamicTable'
+import DebouncedInput from '../../components/reuse/DebouncedInput'
 
 const MehediPage = () => {
     return (
         <div>
-            <DynamicTable/>
+            {/* <DynamicTable/> */}
+            <DebouncedInput/>
         </div>
     );
 };
