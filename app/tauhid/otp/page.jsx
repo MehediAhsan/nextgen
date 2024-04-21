9f5a70e7798c74c9c1d980bc485a83ca01d40ab7
Your Name <tauhid.swe@gmail.com>
2024-04-21 15:00:18 +0600

    update

--- app/tauhid/otp/page.jsx
+++ app/tauhid/otp/page.jsx
@@ -1,9 +1,8 @@
 "use client"
 import Image from 'next/image';
-import React, { useRef, useState } from 'react';
-import img from '@/public/assets/otp.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import img from '@/public/assets/otp.png';
 import { useForm } from 'react-hook-form';
-import OTPInput from 'react-otp-input';
 
 const page = () => {
     const { register } = useForm();
@@ -28,15 +27,25 @@ const page = () => {
         }
     };
 
+    const handlePaste = (e) => {
+        e.preventDefault();
+        const pastedData = e.clipboardData.getData('text');
+        if (/^\d{4}$/.test(pastedData)) {
+            const newOtp = pastedData.split('');
+            setOtp(newOtp);
+        }
+    }
+
+
     return (
-        <div className='bg-white flex flex-row justify-around items-center'>
-            <div className='text-center p-5 space-y-5 bg-gray-50'>
+        <div className='bg-white flex flex-row justify-around items-center '>
+            <div className='text-center p-5 space-y-5 bg-gray-50 w-1/2'>
                 <h2 className='text-black font-bold text-xl'>
                     আপনার ফোন নম্বরে একটি OTP পাঠানো হয়েছে যা আপনার অ্যাকাউন্ট নিবন্ধনের জন্য ব্যবহৃত হবে।
                 </h2>
-                <Image src={img} alt='phone otp image' className='object-cover w-full mx-auto' />
+                <Image src={img} alt='phone_otp_image' className='object-cover w-full' />
             </div>
-            <div className='text-center p-5 flex flex-col space-y-10'>
+            <div className='text-center p-5 flex flex-col space-y-10 w-1/2'>
                 <h1 className='text-black text-2xl font-bold'>OTP ভেরিফিকেশন</h1>
                 <p className='text-black text-sm'>
                     4-সংখ্যার একটি 1কোড লিখুন যা আপনার <br /> <span className='text-blue-400'> +880129234</span> নাম্বারে পাঠানো হয়েছে।
@@ -51,6 +60,7 @@ const page = () => {
                                 value={digit}
                                 onChange={(e) => handleInputChange(e, index)}
                                 onKeyDown={(e) => handleKeyDown(e, index)}
+                                onPaste={index === 0 ? handlePaste : null}
                                 ref={(input) => (inputRefs.current[index] = input)}
                                 className="text-black w-full text-2xl text-center border-b-2 border-gray-300 py-1 focus:border-b-2 focus:border-blue-700 focus:text-blue-700 transition-colors focus:outline-none peer bg-inherit"
                             />)
@@ -61,7 +71,7 @@ const page = () => {
                 <p className='text-sm text-black'>
                     কোড কি পাননি?
                 </p>
-                <h2 className='text-blue-700 underline font-semibold'>পুনরায় কোড পাঠান</h2>
+                <h2 className='text-[#2C74DC] underline font-semibold'>পুনরায় কোড পাঠান</h2>
             </div>
         </div>
     );
--- app/tauhid/payment/page.jsx
+++ app/tauhid/payment/page.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import img from "@/public/assets/payment.svg";
+import img from "@/public/assets/payment.png";
 import logo from "@/public/assets/logo.svg";
 import Image from 'next/image';
 import { TiLockClosed } from "react-icons/ti";
 import bkash from '@/public/assets/bkash.png';
+import nagad from '@/public/assets/nagad.png';
+import dutchBangla from '@/public/assets/dutch-bangla.png';
+import masterCard from '@/public/assets/Mastercard_logo.png';
+import visaCard from '@/public/assets/visa-icon.png';
+
+import { FiPhoneCall } from 'react-icons/fi';
+
+
 
 const page = () => {
     return (
         <div>
-            <div className='bg-gradient-to-r from-white to-blue-200 p-4 hidden md:block text-lg font-semibold text-center'>
+            <div className='bg-gradient-to-r from-white to-[#B8E0FF] p-4 hidden md:block text-lg font-semibold text-center'>
                 <div className='flex justify-around'>
-                    <div className='flex-initial w-[66%] flex items-center justify-around'>
+                    <div className='flex-initial w-[66%] flex items-center justify-around leading-10'>
                         <Image src={logo} className='h-32 w-32' />
                         <h1>
                             নিরাপদ এবং নিরবচ্ছিন্ন অর্থপ্রদান আপনার জন্য অপেক্ষা করছে! <br /> আমাদের বিশ্বস্ত পেমেন্ট গেটওয়ের সাথে ঝামেলা মুক্ত <br /> লেনদেনের অভিজ্ঞতা নিন।
@@ -20,8 +28,8 @@ const page = () => {
                 </div>
             </div>
 
-            <div className='mt-10 flex justify-around'>
-                <div className='w-[33%] border border-gray-700 py-5 px-10 rounded-xl'>
+            <div className='my-10 flex justify-around'>
+                <div className='w-[33%] border h-full border-gray-700 py-5 px-10 rounded-xl'>
                     <h2 className='font-semibold'>পেমেন্ট বিবরণ</h2>
                     <div className='flex justify-between text-sm py-6'>
                         <p>কোম্পানি নিবন্ধন ফি</p>
@@ -34,18 +42,18 @@ const page = () => {
                     </div>
                     <div className='flex justify-between text-sm'>
                         <p>সার্ভিস চার্জ ২%</p>
-                        <p>৳ ১০০০</p>
-                    </div>
-                    <hr className='my-2 border border-gray-200' />
-                    <div className='flex justify-between text-sm py-6'>
-                        <p>সর্বমোট (ভ্যাট সহ) </p>
                         <p>৳ ২০</p>
                     </div>
+                    <hr className='my-2 border border-gray-200' />
+                    <div className='flex justify-between text-sm py-6 font-bold'>
+                        <p>সর্বমোট <span className='font-normal'>(ভ্যাট সহ)</span> </p>
+                        <p>৳ ১০২০</p>
+                    </div>
                 </div>
-                <form action="" className='text-sm border border-gray-700 rounded-xl px-10 py-5 space-y-5'>
+                <form action="" className='text-sm border border-gray-700 rounded-xl px-10 py-5 space-y-8'>
                     <div className='flex justify-between items-center gap-20'>
                         <h6>পেমেন্ট মেথড সিলেক্ট করুন</h6>
-                        <div className='flex items-center gap-2 bg-gray-100 py-2 px-5 rounded-full'>
+                        <div className='flex items-center gap-2 bg-[#D9D9D9] py-2 px-5 rounded-full'>
                             <p className='bg-blue-400 text-white w-7 h-7 p-1 rounded-full text-center'>
                                 <TiLockClosed className='h-5 w-5' />
                             </p>
@@ -54,15 +62,48 @@ const page = () => {
                     </div>
                     <div className='flex items-center justify-between p-4 border border-gray-700 rounded-lg'>
                         <div className='space-x-5'>
-                            <input type="radio" name="bkash" id="bkash" />
+                            <input type="radio" name="bkash" id="bkash" className='w-4 h-4' />
                             <label htmlFor="bkash">বিকাশ</label>
                         </div>
                         <Image src={bkash} className='w-32' />
                     </div>
-                    <div className='space-x-5'>
-                        <input type="checkbox" name="savePhoneNumber" id="savePhoneNumber" />
+                    <div className='space-x-5 flex items-center ms-5'>
+                        <input type="checkbox" name="savePhoneNumber" id="savePhoneNumber" className='w-4 h-4' />
                         <label htmlFor="savePhoneNumber">বিকাশ ফোন নাম্বারটি সেভ করুন</label>
                     </div>
+
+                    <div className='flex items-center justify-between p-4 border border-gray-700 rounded-lg'>
+                        <div className='space-x-5'>
+                            <input type="radio" name="otherPayment" id="otherPayment" className='w-4 h-4' />
+                            <label htmlFor="otherPayment">অন্যান্য পেমেন্ট মাধ্যম</label>
+                        </div>
+                        <div className='flex gap-3'>
+                            <Image src={nagad} className='w-8' />
+                            <Image src={dutchBangla} className='w-6' />
+                            <Image src={visaCard} className='w-10' />
+                            <Image src={masterCard} className='w-10' />
+                        </div>
+                    </div>
+
+                    <div className='flex items-center justify-between p-4 border border-gray-700 rounded-lg'>
+                        <div className='flex gap-5 items-center'>
+                            <FiPhoneCall size={24} className='text-blue-600' />
+                            <p>এজেন্টের সহায়তা নিতে কল করুন 16000 নম্বরে</p>
+                        </div>
+                    </div>
+
+                    <div className='space-x-5 flex items-center ms-5'>
+                        <input type="checkbox" name="agreementStatus" id="agreementStatus" className='w-4 h-4' />
+                        <label htmlFor="agreementStatus">আমি এই প্ল্যাটফর্ম
+                            <a href="#" className='text-blue-600 hover:underline' target='_blank'> ব্যবহারের শর্তাবলি </a>ও 
+                            <a href="#" className='text-blue-600 hover:underline' target='_blank'> গোপনীয়তা নীতির </a>ব্যাপারে সম্মতি দিচ্ছি
+                        </label>
+                    </div>
+
+                    <button type='submit' className='flex items-center justify-between w-full bg-[#2C74DC] p-3 text-white font-semibold rounded-lg'>
+                        <h2>পেমেন্ট করুন</h2>
+                        <p>৳ ১০২০</p>
+                    </button>
                 </form>
             </div>
         </div>
