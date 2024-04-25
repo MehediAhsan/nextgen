418318c3e7f4985ddc366e234e328bfd53c14541
MehediAhsan <mdmehedicse01@gmail.com>
2024-04-25 14:41:31 +0600

    updated product image

--- app/mehedi/productDetails/ProductImages.jsx
+++ app/mehedi/productDetails/ProductImages.jsx
@@ -14,7 +14,6 @@ import 'swiper/css/thumbs';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
 import Image from 'next/image';
-// import '../../styles/ProjectDetailsStyle.css'
 import Zoom from 'react-img-zoom'
 
 const ProductImages = () => {
@@ -23,31 +22,18 @@ const ProductImages = () => {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
     const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768); // Adjust the threshold as needed
-        };
-
-        handleResize(); // Initial check
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
     const images = [
         {
-            thumbnail: "https://images.unsplash.com/photo-1614681792161-e883a0380275?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            thumbnail: "https://images.unsplash.com/photo-1547949003-9792a18a2601?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
             thumbnail: "https://images.unsplash.com/photo-1616534900631-c875e41ab2c8?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            thumbnail: "https://images.unsplash.com/photo-1614681792161-e883a0380275?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            thumbnail: "https://images.unsplash.com/photo-1583947215259-38e31be8751f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            thumbnail: "https://images.unsplash.com/photo-1614681792161-e883a0380275?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            thumbnail: "https://images.unsplash.com/photo-1559056199-641a0ac8b55e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
             thumbnail: "https://images.unsplash.com/photo-1614681792161-e883a0380275?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -78,6 +64,19 @@ const ProductImages = () => {
         );
     }
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768); // Adjust the threshold as needed
+        };
+
+        handleResize(); // Initial check
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
             <div className='overflow-hidden project'>
@@ -102,7 +101,7 @@ const ProductImages = () => {
                                     <Zoom // Replace Image with ReactImgZoom
                                         img={img.thumbnail} // Image source
                                         zoomScale={3} // Zoom scale
-                                        width={600} // Image width
+                                        width={620} // Image width
                                         height={400} // Image height
                                         className='w-full h-96' // Additional classes
                                     />
@@ -137,7 +136,7 @@ const ProductImages = () => {
                 style={{ display: isLightbox ? "flex" : "none" }}
                 className="flex flex-col items-center justify-center group-hover:block fixed inset-0 bg-black bg-opacity-60 max-sm:justify-start z-50"
             >
-                <div className="">
+                <div>
                     <BsFillCaretLeftFill onClick={prevImage} className="hover:opacity-80 cursor-pointer rounded-full w-fit h-[54px] absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 text-white" />
                     <BsCaretRightFill onClick={nextImage} className="hover:opacity-80 cursor-pointer rounded-full w-fit h-[54px] absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 text-white" />
                     <ImCross onClick={() => handleClick(0)} className="absolute top-0 cursor-pointer z-50 right-0 bg-red-500 p-2 text-3xl text-white" />
